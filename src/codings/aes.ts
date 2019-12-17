a32ac1a58a6b6d280b8fe3976eb74436829a5f34
Ngô Xuân Bách <thanbaiks@gmail.com>
2019-12-17 22:35:35 +0700

    fix: Minor fixes

--- src/codings/aes.ts
+++ src/codings/aes.ts
@@ -1,21 +1,20 @@
-import { AES, lib, enc, PBKDF2, WordArray } from 'crypto-js';
-import lzstring from 'lz-string';
+import { AES, enc, PBKDF2, WordArray } from 'crypto-js';
 import Coding from './coding';
 
-const iv = enc.Hex.parse('101112131415161718191a1b1c1d1e1f');
+const godKey = enc.Hex.parse('2136afab11e4138be86328bfff963682');
 
 class AesCoding implements Coding {
   private key: WordArray;
   constructor(key: string) {
-    this.key = PBKDF2(key, iv);
+    this.key = PBKDF2(key, godKey);
   }
 
   public encode(s: string): string {
-    return AES.encrypt(s, this.key, { iv }).toString();
+    return AES.encrypt(s, this.key, { iv: godKey }).toString();
   }
 
   public decode(s: string): string {
-    return AES.decrypt(s, this.key, { iv }).toString(enc.Utf8);
+    return AES.decrypt(s, this.key, { iv: godKey }).toString(enc.Utf8);
   }
 }
 
